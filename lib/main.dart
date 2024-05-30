926e4a042f23cb4570c7c56c7095e8b8ec7a21c7
Ahmed ZNOUDA <42349194+Aomine3500@users.noreply.github.com>
2024-05-30 20:18:01 +0100

    Learn Flutter Web App

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Learn Flutter App'),
+              backgroundColor: Colors.lightBlue,
+            ), //AppBar
+            body: Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  border: Border.all(color: Colors.black, width: 2.5),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Colors.black87,
+                        offset: Offset(5, 5),
+                        spreadRadius: 2,
+                        blurRadius: 5),
+                  ]),
+              width: 1895,
+              height: 850,
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                "We are going to learn flutter for web development",
+                style: TextStyle(
+                  color: Colors.blue[400],
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  //backgroundColor: Colors.black87),
+                ),
+              ),
+            )));
+  }
+}
